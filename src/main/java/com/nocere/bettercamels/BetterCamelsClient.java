01099d9f960e2e81a2ea4c7495a503f93f2dae06
sofewhours <lukerob39@outlook.com>
2026-03-08 22:05:13 +0000

    Update .gitignore, LICENSE, and fabric.mod.json; add BetterCamelsClient class

---
+++ src/main/java/com/nocere/bettercamels/BetterCamelsClient.java
@@ -0,0 +1,13 @@
+package com.nocere.bettercamels;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BetterCamelsClient implements ClientModInitializer{
+
+    @Override
+    public void onInitializeClient() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'onInitializeClient'");
+    }
+
+}
