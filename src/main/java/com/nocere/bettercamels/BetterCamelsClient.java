e6777df02f8dd547ba4ec2db4bf7accd9514a0f8
sofewhours <lukerob39@outlook.com>
2026-03-08 23:01:03 +0000

    project final setup with first run

--- src/main/java/com/nocere/bettercamels/BetterCamelsClient.java
+++ src/main/java/com/nocere/bettercamels/BetterCamelsClient.java
@@ -5,9 +5,8 @@ import net.fabricmc.api.ClientModInitializer;
 public class BetterCamelsClient implements ClientModInitializer{
 
     @Override
-    public void onInitializeClient() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'onInitializeClient'");
+    public void onInitializeClient() { 
+       System.out.println("Better Camels client initialized!"); 
     }
 
 }
