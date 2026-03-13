6f124fb3a3a6c012398fa3fbaab179dddf3643ca
sofewhours <lukerob39@outlook.com>
2026-03-13 01:24:26 +0000

    Added base items for all armour types Item textures need to be added

--- src/main/java/com/nocere/bettercamels/items/ModItems.java
+++ src/main/java/com/nocere/bettercamels/items/ModItems.java
@@ -1,19 +1,45 @@
 package com.nocere.bettercamels.items;
 import com.nocere.bettercamels.BetterCamels;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.Identifier;
 
 public class ModItems {
-    public static final Item LEATHER_CAMEL_ARMOUR_ITEM = registerItem("leather_camel_armour_item", new Item(new Item.Settings()));
+    public static Item LEATHER_CAMEL_ARMOUR_ITEM;
+    public static Item IRON_CAMEL_ARMOUR_ITEM;
+    public static Item GOLD_CAMEL_ARMOUR_ITEM;
+    public static Item DIAMOND_CAMEL_ARMOUR_ITEM;
+    public static Item NETHERITE_CAMEL_ARMOUR_ITEM;
 
-    private static Item registerItem(String name, Item item){
-        return Registry.register(Registries.ITEM, Identifier.of(BetterCamels.MOD_ID, name), item);
+    private static Item registerItem(String name){
+        Identifier id = Identifier.of(BetterCamels.MOD_ID, name);
+        RegistryKey<Item> key = RegistryKey.of(RegistryKeys.ITEM, id);
+        Item.Settings settings = new Item.Settings().registryKey(key);
+
+        return Registry.register(Registries.ITEM, key , new Item(settings));
     }
 	
     public static void registerModItems(){
         BetterCamels.LOGGER.info("Registering Mod Items for: " + BetterCamels.MOD_ID);
-    }
 
+        LEATHER_CAMEL_ARMOUR_ITEM = registerItem("leather_camel_armour_item");
+        IRON_CAMEL_ARMOUR_ITEM = registerItem("iron_camel_armour_item");
+        GOLD_CAMEL_ARMOUR_ITEM = registerItem("gold_camel_armour_item");
+        DIAMOND_CAMEL_ARMOUR_ITEM = registerItem("diamond_camel_armour_item");
+        NETHERITE_CAMEL_ARMOUR_ITEM = registerItem("netherite_camel_armour_item");
+
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.COMBAT).register(fabricItemGroupEntries -> {
+            fabricItemGroupEntries.add(LEATHER_CAMEL_ARMOUR_ITEM);
+            fabricItemGroupEntries.add(IRON_CAMEL_ARMOUR_ITEM);
+            fabricItemGroupEntries.add(GOLD_CAMEL_ARMOUR_ITEM);
+            fabricItemGroupEntries.add(DIAMOND_CAMEL_ARMOUR_ITEM);
+            fabricItemGroupEntries.add(NETHERITE_CAMEL_ARMOUR_ITEM);
+        });
+    }
 }
